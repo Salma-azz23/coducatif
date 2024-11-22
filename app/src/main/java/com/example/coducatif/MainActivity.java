13ea630a75b22cf712a17e9388a95824fff88773
chaimaeb627 <chaimaeb627@gmail.com>
2024-11-22 19:21:02 +0100

    PAGE 19

--- app/src/main/java/com/example/coducatif/MainActivity.java
+++ app/src/main/java/com/example/coducatif/MainActivity.java
@@ -1,24 +1,34 @@
-package com.example.coducatif;
-
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.coducatif.Course;
+import com.example.coducatif.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Liste des cours
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course("React js", "Framework of javascript", "7058/-", "⭐ 4.2 | 7830 Std", R.drawable.react_image));
+        courseList.add(new Course("Symfony", "Framework of PHP", "800/-", "⭐ 3.9 | 12680 Std", R.drawable.symfony_image));
+        courseList.add(new Course("Laravel", "Framework of PHP", "599/-", "⭐ 4.1 | 990 Std", R.drawable.laravel_image));
+        courseList.add(new Course("Next.js", "Advanced Frontend Framework", "499/-", "⭐ 4.9 | 14580 Std", R.drawable.next_image));
+
+        // Adaptateur
+        CourseAdapter adapter = new CourseAdapter(courseList);
+        recyclerView.setAdapter(adapter);
     }
 }
