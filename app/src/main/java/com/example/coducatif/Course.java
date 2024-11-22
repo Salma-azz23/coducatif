4ba0ab15885d29458bed8cb46cb737224afe44b2
chaimaeb627 <chaimaeb627@gmail.com>
2024-11-22 19:23:57 +0100

    Ajout de fichiers non suivis avant pull

---
+++ app/src/main/java/com/example/coducatif/Course.java
@@ -0,0 +1,42 @@
+package com.example.coducatif;
+
+public class Course {
+
+        private String category;
+        private String name;
+        private String price;
+        private String rating;
+        private int imageResource;
+
+        // Constructeur
+        public Course(String category, String name, String price, String rating, int imageResource) {
+            this.category = category;
+            this.name = name;
+            this.price = price;
+            this.rating = rating;
+            this.imageResource = imageResource;
+        }
+
+        // Getters
+        public String getCategory() {
+            return category;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public String getRating() {
+            return rating;
+        }
+
+        public int getImageResource() {
+            return imageResource;
+        }
+    }
+
+}
