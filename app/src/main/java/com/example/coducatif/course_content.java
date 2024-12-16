46858589429c1c91433a59dae0a909c1d2aa8fe2
yagoubi oumayma <yagoubi.oumayma20@gmail.com>
2024-12-16 22:51:44 +0100

    update

--- app/src/main/java/com/example/coducatif/course_content.java
+++ app/src/main/java/com/example/coducatif/course_content.java
@@ -14,19 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.example.coducatif.R;
 
+
 public class course_content extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_course_content);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 
     // Méthode appelée lors du clic sur l'image "Enroll Now"
